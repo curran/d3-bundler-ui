4ba523fca1b20cfec71340ef4e6e8976448f1cbe
Curran <curran.kelleher@gmail.com>
2015-07-27 19:25:47 -0700

    Add Express server that parses module list from URL

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
 var fs = require("fs");
 
-// Read modules.json
-var modules = JSON.parse(fs.readFileSync("modules.json", "utf8"));
-
 // Generates the JavaScript code for the top-level D3 bundle.
 // Takes as input an array of module names to include.
 // These module names should correspond with the keys found in the file `modules.json`
-module.exports = function generateIndexJS(modulesToInclude){
-  return [
-    modulesToInclude.map(function (module){
-      return modules[module].import;
-    }).join("\n"),
-    "",
-    "export default {",
-    modulesToInclude.map(function (module){
-      return "  " + modules[module].export;
-    }).join(",\n"),
-    "};"
-  ].join("\n");
+var generateIndexJS = (function (){
+  var modules = JSON.parse(fs.readFileSync("modules.json", "utf8"));
+  return function (modulesToInclude){
+    return [
+      modulesToInclude.map(function (module){
+        return modules[module].import;
+      }).join("\n"),
+      "",
+      "export default {",
+      modulesToInclude.map(function (module){
+        return "  " + modules[module].export;
+      }).join(",\n"),
+      "};"
+    ].join("\n");
+  };
+}());
+
+// Parses a comma-separated list of modules names.
+function parseModulesList(modulesList){
+  return modulesList.split(",").map(function (str){
+    return str.trim();
+  });
+}
+
+var express = require("express");
+var app = express();
+
+app.get("/:modulesList", function (req, res) {
+  console.log(req.params.modulesList);
+  var modulesToInclude = parseModulesList(req.params.modulesList);
+  var indexJS = generateIndexJS(modulesToInclude);
+
+  res.format({
+    text: function(){
+      res.send(indexJS);
+    }
+  });
+});
+
+var server = app.listen(3000, function () {
+  var port = server.address().port;
+  console.log("Head on over to http://localhost:" + port + "/select,event,transition");
+});
+
+module.exports = {
+  generateIndexJS: generateIndexJS,
+  parseModulesList: parseModulesList
 };
--- test.js
+++ test.js
@@ -3,9 +3,10 @@ var d3BundlerUI = require("./index.js");
 var assert = require("assert");
 
 describe("d3-ui-bundler", function () {
+
   it("should generate index.js from a list of modules", function() {
 
-    var indexJS = d3BundlerUI(["select", "event", "transition"]);
+    var indexJS = d3BundlerUI.generateIndexJS(["select", "event", "transition"]);
 
     assert.equal(indexJS, [
       "import { select } from \"d3-selection\";",
@@ -18,5 +19,12 @@ describe("d3-ui-bundler", function () {
       "  transition: transition",
       "};",
     ].join("\n"));
+  });
+
+  it("should parse a comma-separed list of module names", function() {
+
+    var modulesArr = d3BundlerUI.parseModulesList("select,event  , transition");
+
+    assert.equal(modulesArr.join(" "), "select event transition");
   });
 });
